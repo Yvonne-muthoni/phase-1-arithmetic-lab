add7563f567bba0fe0bd6754739090cb10ecc9e7
Yvonne Muriuki <yvonnemuriuki01@gmail.com>
2024-03-23 12:06:39 +0300

    creates index.js file

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+console.log(multiply)
 // Write your code here
+
+const num3 = 14
+const num4 = 5
+const mod = num3 % num4
+mod === 4
+console.log(mod)
+
+const max = Math.max(5,10,20)
+console.log(max)
